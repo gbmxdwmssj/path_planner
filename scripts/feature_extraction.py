d2e63bb638870b4464c3ed2814669eb6a691dba9
KaiZhang <kaizhangbit@gmail.com>
2019-07-01 08:50:33 +0800

    Add motion_prediction.py

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -2,9 +2,12 @@
 
 import rospy
 import math
+import numpy as np
 from scipy.interpolate import interp1d
 from nav_msgs.msg import Path
 from hybrid_astar.srv import *
+from std_msgs.msg import Float64MultiArray
+import pylab as pl
 
 v_0 = 0.0 # m/s
 a_0 = 2.0 # m/s^2
@@ -12,6 +15,43 @@ v_tra = 3.0
 a_f = -2.0
 v_f = 0.0
 t_f = None
+
+def radMinus(rad_1, rad_2): # [0, 2pi)
+    assert (rad_1 >=0 and rad_1 < 2 * math.pi)
+    assert (rad_2 >=0 and rad_2 < 2 * math.pi)
+    abs_diff_1 = abs(rad_1 - rad_2)
+    abs_diff_2 = 2 * math.pi - abs_diff_1
+    if rad_1 >= rad_2 and abs_diff_1 >= abs_diff_2:
+        return -abs_diff_2
+    elif rad_1 >= rad_2 and abs_diff_1 < abs_diff_2:
+        return abs_diff_1
+    elif rad_1 < rad_2 and abs_diff_1 < abs_diff_2:
+        return -abs_diff_1
+    elif rad_1 < rad_2 and abs_diff_1 >= abs_diff_2:
+        return abs_diff_2
+    else:
+        assert False
+
+def t_to_l(t, v_feature):
+    v_0 = v_feature.data[0]
+    a_0 = v_feature.data[1]
+    v_tra = v_feature.data[2]
+    a_f = v_feature.data[3]
+    v_f = v_feature.data[4]
+    t_f = v_feature.data[5]
+    t_1 = (v_tra - v_0) / a_0
+    t_2 = t_f - ((v_f - v_tra) / a_f)
+    trape_1_area = (v_0 + v_tra) * t_1 * 0.5
+    if t <= t_1:
+        t = max(0, t)
+        l = (v_0 + (v_0 + a_0 * t)) * t * 0.5
+    elif t <= t_2:
+        l = trape_1_area + v_tra * (t - t_1)
+    else:
+        t = min(t, t_f)
+        up = v_tra + a_f * (t - t_2)
+        l = trape_1_area + v_tra * (t_2 - t_1) + (up + v_tra) * (t - t_2) * 0.5
+    return l
 
 def normalizedHeadingRad(theta_z):
     if theta_z < 0:
@@ -23,74 +63,89 @@ def normalizedHeadingRad(theta_z):
 def callback(path):
     if len(path.poses) == 0:
         return
-    print('start process!!!!!!')
-    print(len(path.poses))
 
     ## compute the lenght of the path
     l_f = 0.0
     l_list = []
     theta_z_list = []
     euler_from_quaternion = rospy.ServiceProxy('/euler_from_quaternion', EulerFromQuaternion)
-    # for i in range(len(path.poses) - 1):
     for i in range(len(path.poses) - 1, 0, -1):
         l_list.append(l_f)
         
         euler = euler_from_quaternion(path.poses[i].pose.orientation)
         yaw = normalizedHeadingRad(euler.yaw)
         theta_z_list.append(yaw)
-        print(yaw)
 
         x = path.poses[i].pose.position.x
         y = path.poses[i].pose.position.y
         dx = x - path.poses[i-1].pose.position.x
         dy = y - path.poses[i-1].pose.position.y
-        l_f = l_f + math.sqrt(dx*dx + dy*dy)
+        dis = math.sqrt(dx*dx + dy*dy)
+        l_f = l_f + dis
     l_list.append(l_f)
     euler = euler_from_quaternion(path.poses[0].pose.orientation)
     yaw = normalizedHeadingRad(euler.yaw)
     theta_z_list.append(yaw)
-    print('path length')
-    print(l_f)
-    print('l_list')
-    print(l_list)
-    print('theta_z_list')
-    print(theta_z_list)
     l_to_theta_z = interp1d(l_list, theta_z_list, kind='slinear')
+    l_new = np.linspace(0, l_f, 1000)
+    theta_z_new = l_to_theta_z(l_new)
+    print('theta_z_list:')
+    print(theta_z_list)
+    pl.plot(l_new, theta_z_new)
 
     frac_1 = (v_tra - v_0)*(v_tra - v_0) / (2 * a_0)
     frac_2 = (v_tra - v_f)*(v_tra - v_f) / (2 * a_f)
     t_f = (l_f + frac_1 - frac_2) / v_tra
     t_1 = (v_tra - v_0) / a_0
     t_2 = t_f - ((v_f - v_tra) / a_f)
-    print('t_f')
-    print(t_f)
-    print(t_1)
-    print(t_2)
+
+    v_feature = Float64MultiArray()
+    v_feature.data = [v_0, a_0, v_tra, a_f, v_f, t_f]
 
     K = 9
     Delta_t = t_f / (K - 1)
-    trape_1_area = (v_0 + v_tra) * t_1 * 0.5
+    omega_z_feature = Float64MultiArray()
+    dt = rospy.get_param('/hybrid_astar/dt') # s
     for k in range(K):
         t = k * Delta_t
-        if t <= t_1:
-            l = (v_0 + (v_0 + a_0 * t)) * t * 0.5
-        elif t <= t_2:
-            l = trape_1_area + v_tra * (t - t_1)
-        else:
-            up = v_tra + a_f * (t - t_2)
-            l = trape_1_area + v_tra * (t_2 - t_1) + (up + v_tra) * (t - t_2) * 0.5
-        print('l')
-        print(l)
-        if k == K - 1:
-            l = l - 0.000001
+        l = t_to_l(t, v_feature)
         theta_z = l_to_theta_z(l)
-        print('theta_z')
-        print(theta_z)
+        if k == K - 1:
+            l_minus = t_to_l(t - dt, v_feature)
+            l = l - 0.000001
+        else:
+            l_plus = t_to_l(t + dt, v_feature)
+
+        if k == K - 1:
+            theta_z_minus = l_to_theta_z(l_minus)
+            print(theta_z)
+            print(theta_z_minus)
+            omega_z = radMinus(theta_z, theta_z_minus) / dt
+        else:
+            theta_z_plus = l_to_theta_z(l_plus)
+            print(theta_z_plus)
+            print(theta_z)
+            omega_z = radMinus(theta_z_plus, theta_z) / dt
+
+        omega_z_feature.data.append(omega_z)
+
+    print(omega_z_feature.data)
+    feature = Float64MultiArray()
+    tmp_len = len(path.poses)
+    x0 = path.poses[tmp_len-1].pose.position.x
+    y0 = path.poses[tmp_len-1].pose.position.y
+    euler = euler_from_quaternion(path.poses[tmp_len-1].pose.orientation)
+    yaw = normalizedHeadingRad(euler.yaw)
+    theta_z0 = yaw
+    feature.data = [x0, y0, 0, 0, 0, theta_z0] + v_feature.data + omega_z_feature.data
+    pub.publish(feature)
+    pl.show()
 
 rospy.init_node('feature_extraction', anonymous=True)
 print('I waiting for service /euler_from_quaternion...')
 rospy.wait_for_service('/euler_from_quaternion')
 print('Service /euler_from_quaternion is availiable!')
 rospy.Subscriber('/sPath', Path, callback)
+pub = rospy.Publisher('/path_feature', Float64MultiArray, queue_size=10)
 rospy.spin()
 
---
+++ scripts/motion_prediction.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+import numpy as np
+import pylab as pl
+from std_msgs.msg import Float64MultiArray
+from scipy.interpolate import interp1d
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+# class State:
+#     def __init__(self, x, y, z, theta_x, theta_y, theta_z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#         self.theta_x = theta_x
+#         self.theta_y = theta_y
+#         self.theta_z = theta_z
+
+def normalizedHeadingRad(theta_z):
+    if theta_z < 0:
+        theta_z = theta_z - 2.0 * math.pi * (int)(theta_z / (2.0 * math.pi))
+        return 2.0 * math.pi + theta_z
+
+    return theta_z - 2.0 * math.pi * (int)(theta_z / (2.0 * math.pi))
+
+def f(s, t, v, omega_z):
+    c_x = math.cos(s[3])
+    s_x = math.sin(s[3])
+    c_y = math.cos(s[4])
+    s_y = math.sin(s[4])
+    c_z = math.cos(s[5])
+    s_z = math.sin(s[5])
+    mat_1 = np.matrix(np.array([[c_z*c_y, c_z*s_y*s_x-s_z*c_x, 0.0     ],
+                                [s_z*c_y, s_z*s_y*s_x-c_z*c_x, 0.0     ],
+                                [0.0,     0.0,                 c_x/c_y]]))
+    mat_2 = np.matrix(np.array([v, 0.0, omega_z]))
+    return np.array(mat_1 * mat_2.T)
+
+
+
+def t_to_v(t, feature):
+    v_0 = feature.data[6]
+    a_0 = feature.data[7]
+    v_tra = feature.data[8]
+    a_f = feature.data[9]
+    v_f = feature.data[10]
+    t_f = feature.data[11]
+
+    t_1 = (v_tra - v_0) / a_0
+    t_2 = t_f - ((v_f - v_tra) / a_f)
+    if t <= t_1:
+        t = max(0, t)
+        return v_0 + a_0 * t
+    elif t <= t_2:
+        return v_tra
+    else:
+        t = min(t, t_f)
+        return v_tra + a_f * (t - t_2)
+
+
+
+def callback(feature):
+    print('I get a path feature!')
+    t_f = feature.data[11]
+    t_list = []
+    omega_z_list = []
+    K = len(feature.data) - 12
+    Delta_t = t_f / (K - 1)
+    for k in range(K):
+        t_list.append(k * Delta_t)
+
+    for i in range(12, len(feature.data), 1):
+        omega_z_list.append(feature.data[i])
+
+    # print(t_list)
+    # print(omega_z_list)
+    t_to_omega_z = interp1d(t_list, omega_z_list, kind='cubic')
+    t_new = np.linspace(0, t_f, 1000)
+    omega_z_new = t_to_omega_z(t_new)
+    pl.plot(t_new, omega_z_new)
+
+    h = rospy.get_param('/hybrid_astar/dt') # s
+    t = 0
+    s = [feature.data[0], feature.data[1], feature.data[2],
+        feature.data[3], feature.data[4], feature.data[5]]
+    s_list = []
+    s_list.append(s)
+    while t <= t_f:
+        arr = f(s, t, t_to_v(t, feature), t_to_omega_z(t))
+        # print(arr[0,0]*h)
+        s = [s[0] + arr[0,0]*h, s[1] + arr[1,0]*h, s[2] + 0,
+             s[3] + 0,          s[4] + 0,          normalizedHeadingRad(s[5] + arr[2,0]*h)]
+        s_list.append(s)
+        t = t + h
+    # print(s_list)
+
+    predicted_path = Path()
+    predicted_path.header.frame_id = 'map'
+    for s in s_list:
+        pose = PoseStamped()
+        pose.pose.position.x = s[0]
+        pose.pose.position.y = s[1]
+        pose.pose.orientation.w = 1.0
+        predicted_path.poses.append(pose)
+    pub.publish(predicted_path)
+    # pl.show()
+
+
+
+rospy.init_node('motion_prediction', anonymous=True)
+rospy.Subscriber('/path_feature', Float64MultiArray, callback)
+pub = rospy.Publisher('/predicted_path', Path, queue_size=10)
+rospy.spin()
