a699202815e466e220b4ca30cc9dd8293b6778d8
KaiZhang <kaizhangbit@gmail.com>
2019-07-14 20:43:17 +0800

    Complete spline.py

--- scripts/spline.py
+++ scripts/spline.py
@@ -11,6 +11,63 @@ import pylab as pl
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
+import random
+from geometry_msgs.msg import PoseStamped
+
+
+
+def getDisReso(path):
+    x = path[0].pose.position.x
+    y = path[0].pose.position.y
+    dx = x - path[1].pose.position.x
+    dy = y - path[1].pose.position.y
+    dis = math.sqrt(dx*dx + dy*dy)
+    return dis
+
+
+
+def getLength(path):
+    l_f = 0.0
+    for i in range(len(path) - 1):
+        x = path[i].pose.position.x
+        y = path[i].pose.position.y
+        dx = x - path[i+1].pose.position.x
+        dy = y - path[i+1].pose.position.y
+        dis = math.sqrt(dx*dx + dy*dy)
+        l_f = l_f + dis
+    return l_f
+
+
+
+def getKnots(path):
+    x_list = []
+    y_list = []
+    min_knot_num = 4
+    min_dis_step = 5.0 # m
+    max_dis_step = getLength(path) / (min_knot_num - 1) # m
+    min_idx_step = int(min_dis_step / getDisReso(path))
+    max_idx_step = int(max_dis_step / getDisReso(path))
+    min_idx_step = max(1, min_idx_step)
+    max_idx_step = max(1, max_idx_step)
+    print(min_idx_step)
+    print(max_idx_step)
+    min_idx_step = min(min_idx_step, max_idx_step)
+    idx = 0
+    idx_list = []
+    while idx < len(path):
+        x_list.append(path[idx].pose.position.x)
+        y_list.append(path[idx].pose.position.y)
+        rand_tmp = random.randint(min_idx_step, max_idx_step)
+        idx += rand_tmp
+    didx = (len(path) - 1) - (idx - rand_tmp)
+    if didx < min_idx_step:
+        x_list[len(x_list)-1] = path[len(path)-1].pose.position.x
+        y_list[len(y_list)-1] = path[len(path)-1].pose.position.y
+    else:
+        x_list.append(path[len(path)-1].pose.position.x)
+        y_list.append(path[len(path)-1].pose.position.y)
+
+    return [x_list, y_list]
 
 
 
@@ -32,46 +89,38 @@ def callback(path):
     single_path.append(path.poses[0])
     if len(single_path) >= 2:
         paths.append(single_path)
-    # print('number of paths:')
-    # print(len(paths))
 
     fea_list = []
     dims = []
     data_offset = 0
+    whole_path = Path()
+    whole_path.header.frame_id = 'path'
     for path in paths:
-        fea = calcFeature(path)
-        fea_list = fea_list + fea.data
-        print(len(fea.data))
-        data_offset += len(fea.data)
-        single_dim = MultiArrayDimension()
-        single_dim.stride = data_offset
-        dims.append(single_dim)
-    print('number of features:')
-    print(len(fea_list))
-    # print(fea_list)
-    features = Float64MultiArray()
-    features.data = fea_list
-    features.layout.dim = dims
-    pub.publish(features)
+        knots = getKnots(path)
+        m = len(knots[0])
+        if m <= 1:
+            continue
+        tck, u = interpolate.splprep(knots, s=0, k=min(3, m-1))
+        size = int(getLength(path) / getDisReso(path) + 0.5)
+        unew = np.arange(0, 1.0 + 1.0/size, 1.0/size)
+        out = interpolate.splev(unew, tck)
+
+        optim_path = Path()
+        optim_path.header.frame_id = 'path'
+        for i in range(len(out[0])):
+            pose = PoseStamped()
+            pose.pose.position.x = out[0][i]
+            pose.pose.position.y = out[1][i]
+            optim_path.poses.append(pose)
+        whole_path.poses += optim_path.poses
+
+    pub.publish(whole_path)
+        
 
 
 
 rospy.init_node('spline', anonymous=True)
-# print('I waiting for service /euler_from_quaternion...')
-# rospy.wait_for_service('/euler_from_quaternion')
-# print('Service /euler_from_quaternion is availiable!')
 rospy.Subscriber('/sPath', Path, callback)
 pub = rospy.Publisher('/oPath', Path, queue_size=10)
-
-x = [0, 8,  8,  4,  5,  9]
-y = [0, 2, 10, 11, 18, 23]
-tck, u = interpolate.splprep([x, y], s=0)
-unew = np.arange(0, 1.01, 0.01)
-out = interpolate.splev(unew, tck)
-plt.figure()
-plt.plot(x, y, 'x', out[0], out[1])
-plt.show()
-print(u)
-
 print('I will spin!')
-# rospy.spin()
+rospy.spin()
